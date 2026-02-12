159a81d107165a1ccbfeb337d0fe3de8c789757a
finnimon <finnimon01@gmail.com>
2026-02-12 13:02:53 +0100

    perf + test

---
+++ MeshWiz.Buffers.UnitTests/PoolGcSafety.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace MeshWiz.Buffers.UnitTests;
+
+public class PoolGcSafety
+{
+    [Test]
+    public void RentReturnCollect()
+    {
+        var weakRef=WriteToPoolBuf();
+        GC.Collect(GC.MaxGeneration,GCCollectionMode.Default,blocking:true);
+        // using var buf = Pool.Rent<string>(1);
+        Assert.That(weakRef.IsAlive,Is.False);
+    }
+
+    private static WeakReference WriteToPoolBuf()
+    {
+        using var b = Pool.Rent<string>(1);
+        
+        {
+            b.Span[0] = new Random().Next().ToString();
+        }
+        
+        GC.Collect(GC.MaxGeneration, GCCollectionMode.Default,blocking:true);
+        var weakRef= new WeakReference(b.Span[0]);
+        Assert.That(weakRef.IsAlive,Is.True);
+        return weakRef;
+    }
+}
